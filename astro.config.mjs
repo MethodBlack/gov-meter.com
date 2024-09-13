5093a9a4c2864ff9d6666d570195a93bd9b66128
planetabhi <theplanetabhi@gmail.com>
2024-09-13 20:24:46 +0530

    ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import db from '@astrojs/db';
 import { defineConfig } from 'astro/config';
+import deno from "@deno/astro-adapter";
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.URL || 'https://clock.netlify.app/',
+	site: process.env.URL || 'https://gov-meter.deno.dev',
 	integrations: [db()],
+	output: "server",
+  	adapter: deno(),
 });
