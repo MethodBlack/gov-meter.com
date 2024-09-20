e095394d5e08cd76ced1e07af1287b939e313783
planetabhi <theplanetabhi@gmail.com>
2024-09-21 03:19:15 +0530

    ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
-  site: process.env.URL || 'https://meter.netlify.app',
   integrations: [db()],
   output: 'server',
   adapter: netlify(),
