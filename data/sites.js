f2674bc57bcc529b207961f1d67b8120427d68da
planetabhi <theplanetabhi@gmail.com>
2024-09-29 16:42:56 +0530

    setup

--- data/sites.js
+++ data/sites.js
@@ -125,7 +125,8 @@ module.exports = {
 			'https://ibm.gov.in/index.php?c=pages&m=index&id=224',
 			'https://ddpdoo.gov.in/units/NADP',
 			'https://ceeindia.org/envis',
-			'https://birdsjute.in/'
+			'https://birdsjute.in/',
+			'https://rrcnes.gov.in/'
 		]);
 
 		const fetch = (await import('node-fetch')).default;
